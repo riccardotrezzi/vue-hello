e5001823f9a084e2c0a85413e86376677276f0ed
riccardotrezzi <riccardo.trezzi00@gmail.com>
2024-08-29 10:10:53 +0200

    Esercizio finito ma da sistemare

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,12 @@
-console.log('JS Collegato')
+const { createApp } = Vue;  // Desctructuring
+
+const app = createApp({              // Serve per creare un oggetto per l'istanza di Vue
+    data() {
+        return {
+            message: 'Hello Vue!',
+            imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/9/95/Vue.js_Logo_2.svg'
+        }
+    }
+});
+    
+app.mount('#app');
